15b31015554683844a880603534e0c1f9b178e63
petezim05 <petezim05@gmail.com>
2026-03-04 15:56:52 -0600

    modified main to adjust training examples to user input

--- main.py
+++ main.py
@@ -117,5 +117,7 @@ def train(games, net, depth, batch):
             torch.save(net.state_dict(), f'chess_model_{i}.pth')
             print(f"game {i}, loss: {batchLoss}")
 
+games = input("enter number of games: ")
+batches = input("enter number of batches")
 
-train(games=100, net=net, depth=1, batch=50)
+train(games=games, net=net, depth=1, batch=batches)
